94b9d57d33e0e4100054da0a5a045f4366bbf029
ging3ris3pic <ging3ris3pic@gmail.com>
2023-04-28 14:12:20 -0700

    Lesson 5 final

---
+++ src/MultipleLettersException.java
@@ -0,0 +1,8 @@
+public class MultipleLettersException extends Exception{
+
+    @Override
+    public String getMessage(){
+        String errorMsg = "More than one letter was entered";
+        return errorMsg;
+    }
+}
